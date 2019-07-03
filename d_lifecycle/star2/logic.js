4f875afcc18f7d53b14d7828d1525b9fa64aa6d5
Sapir Malka <sapirm290@gmail.com>
2019-07-03 09:33:29 +0300

    Clener code for the app

--- d_lifecycle/star2/logic.js
+++ d_lifecycle/star2/logic.js
@@ -1,33 +1,25 @@
 const App = () => {
   const [answers, setAnswers] = React.useState({ general: "", java: "", python: "" })
   const handleChange = (e) => {
-    let newAnswers = {};
+    let newAnswers = { ...answers, [e.target.name]: e.target.value };
     if (e.target.name === "general" && e.target.value === "no") {
-      for (const key in newAnswers) {
-        newAnswers[key] = "";
-      }
+      newAnswers = { general: "no", java: "", python: '' };
     }
-    else {
-      for (const key in answers) {
-        newAnswers[key] = answers[`${key}`];
-      }
-    }
-    newAnswers[e.target.name] = e.target.value;
     setAnswers(newAnswers);
   }
   return (
     <div>
-      <Question currentState={answers["general"]} topic="general" contect="Have you programmed Before?" handleChange={handleChange}></Question>
-      <Question currentState={answers["java"]} topic="java" contect="Have you programmed in java?" handleChange={handleChange}></Question>
-      <Question currentState={answers["python"]} topic="python" contect="Have you programmed in Python?" handleChange={handleChange}></Question>
+      <Question currentState={answers["general"]} topic="general" content="Have you programmed Before?" handleChange={handleChange}></Question>
+      <Question currentState={answers["java"]} topic="java" content="Have you programmed in java?" handleChange={handleChange}></Question>
+      <Question currentState={answers["python"]} topic="python" content="Have you programmed in Python?" handleChange={handleChange}></Question>
     </div>
   )
 }
 
-const Question = ({ contect, topic, currentState, handleChange }) => {
+const Question = ({ content, topic, currentState, handleChange }) => {
   return (
     <div>
-      <h1>{contect}</h1>
+      <h1>{content}</h1>
       Yes<input type="radio" name={topic} checked={currentState == "yes"} onChange={handleChange} value="yes" />
       No<input type="radio" name={topic} checked={currentState == "no"} onChange={handleChange} value="no" />
     </div>
